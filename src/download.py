2b7091dee1a0ef614552cf2f21ac6da9ab391838
ARTHUR RABELO DE CARVALHO <163140240+arthurabelo@users.noreply.github.com>
2024-08-11 18:04:11 -0300

    Fusão entre aqrquivos saveimage e download

--- src/download.py
+++ src/download.py
@@ -1,17 +1,30 @@
 import requests
-from tqdm import tqdm  # progress bar
 import os
+import base64
+import re
+from tqdm import tqdm
 
 class Download:
-    def __init__(self, url, path_arquivo):
+    def __init__(self, url=None, path_arquivo=None):
         self.url = url
         self.path_arquivo = path_arquivo
-        self.callback = None # Function to be called for progress updates
+        self.callback = None  # Function to be called for progress updates
 
     def set_callback(self, callback):
         self.callback = callback
 
     def executa(self):
+        if self.url:
+            if self.url.startswith("http://") or self.url.startswith("https://"):
+                return self.download_from_url()
+            elif self.url.startswith("data:image/"):
+                return self.save_image_from_base64()
+        elif self.path_arquivo:
+            return self.save_local_file()
+        else:
+            raise Exception("Nenhuma URL ou caminho de arquivo fornecido.")
+
+    def download_from_url(self):
         try:
             print('Aguarde...')
             response = requests.get(self.url, stream=True)  # Habilita streaming para progresso
@@ -33,3 +46,45 @@ class Download:
         except requests.exceptions.RequestException as e:
             print(f"Erro na conexão: {e}")
             raise Exception(f"Erro na conexão: {e}")
+        
+    def get_next_image_number(directory):
+        existing_files = os.listdir(directory)
+        image_number = 0
+        for f in existing_files: # Percorre os arquivos do diretório imagens
+            match = re.search(r'image-(\d+)', f)
+            if match: # Verifica apenas os arquivos iniciados por "image-"
+                image_number += 1
+        return image_number + 1
+
+    def save_local_file(self):
+        try:
+            directory = "imagens"
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            next_number = self.get_next_image_number(directory)
+            file_name = f"image-{next_number}.{file_extension}"
+            destination_path = os.path.join(directory, file_name)
+            with open(self.path_arquivo, 'rb') as file:
+                with open(destination_path, 'wb') as new_file:
+                    new_file.write(file.read())
+            return destination_path
+        except Exception as e:
+            raise Exception(f"Erro ao salvar arquivo local: {e}")
+
+    def save_image_from_base64(self):
+        # Imagem em base64
+        try:
+            header, encoded = self.url.split(",", 1)
+            file_extension = header.split("/")[1].split(";")[0]
+            directory = "imagens"
+            next_number = get_next_image_number(directory)
+            file_name = f"image-{next_number}.{file_extension}"
+            file_path = os.path.join(directory, file_name)
+            
+            with open(file_path, 'wb') as file:
+                file.write(base64.b64decode(encoded))
+                
+            return file_path
+        
+        except Exception as e:
+            raise Exception(f"Erro ao salvar imagem base64: {e}")
